660ffa2d3f6bf9f5c5bca279cb99686d021f9ddb
Kenny Devin <kendevxz@gmail.com>
2023-08-21 14:02:49 +0700

    web structure done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  get 'product', to: 'pages#product', as: 'product'
+  get 'gallery', to: 'pages#gallery', as: 'gallery'
+  get 'contact', to: 'pages#contact', as: 'contact'
 end
